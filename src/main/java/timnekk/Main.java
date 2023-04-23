33cd1e830eba91344f40bac5856ee2bfe18d73c5
TimNekk <herew26@gmail.com>
2023-04-23 13:58:46 +0300

    feat: set up main as command

--- src/main/java/timnekk/Main.java
+++ src/main/java/timnekk/Main.java
@@ -3,21 +3,44 @@ package timnekk;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.ParameterException;
+import picocli.CommandLine.Help;
+
 import timnekk.exceptions.GameFlowException;
 import timnekk.handlers.InputHandler;
 import timnekk.handlers.OutputHandler;
 import timnekk.services.App;
 
-public final class Main {
+@Command(name = "quizhero", mixinStandardHelpOptions = true, description = "Quiz Hero", requiredOptionMarker = '*', abbreviateSynopsis = true)
+public final class Main implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
-        try (App app = new App(new InputHandler(System.in), new OutputHandler(System.out))) {
+    @Option(names = { "-Q",
+            "--questionsPerRequest" }, description = "Amount of questions that will be fetched with single request", defaultValue = "10", showDefaultValue = Help.Visibility.ALWAYS)
+    private int questionsPerRequest;
+
+    @Override
+    public void run() {
+        try (App app = new App(new InputHandler(System.in), new OutputHandler(System.out), questionsPerRequest)) {
             app.run();
         } catch (GameFlowException e) {
             logger.error("Error while running app", e);
+        } catch (IllegalArgumentException e) {
+            logger.error("Wrong arguments: {}", e.getMessage());
         }
+    }
 
-        logger.info("App finished");
+    public static void main(String[] args) {
+        final CommandLine cmd = new CommandLine(new Main());
+
+        try {
+            cmd.parseArgs(args);
+            cmd.execute(args);
+        } catch (ParameterException e) {
+            logger.error("Parsing command line arguments failed: {}", e.getMessage());
+        }
     }
 }
